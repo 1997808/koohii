8efc96c590a0dfa630a5dad531c1f276aea38438
1997808 <khanhhuyle22@gmail.com>
2019-10-24 12:48:14 +0700

    lul

---
+++ JS/Responsive.js
@@ -0,0 +1,10 @@
+window.addEventListener('resize',function(){
+    /*
+    let div = document.querySelectorAll('.footCol');
+    if (window.innerWidth < 768) {
+        div[0].classList.add('d-none');
+    } else {
+        div[0].classList.remove('d-none');
+    }
+    */
+})
